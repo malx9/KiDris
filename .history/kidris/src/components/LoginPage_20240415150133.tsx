b2f573680418cf6796a544d8886c13574b906b86
malx9 <144179748+malx9@users.noreply.github.com>
2024-04-17 15:24:22 +0100

    login page layout changes and pw showing functionality

--- .history/kidris/src/components/LoginPage_20240415150133.tsx
+++ .history/kidris/src/components/LoginPage_20240415150133.tsx
@@ -1,12 +1,4 @@
-import { useState } from "react";
-
 const LoginPage = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
     <>
       <div className="app-title font-inika text-center pt-12 leading-10">
@@ -20,24 +12,13 @@ const LoginPage = () => {
               <h6 className="font-inter text-[#73008F] text-[.8125rem]">
                 Username
               </h6>
-              <input
-                type="text"
-                className="rounded-[3px] h-[45px] focus:outline-none focus:border-[2px] focus:border-[#72008f4f] pl-3 text-[13px] font-inter font-[500] caret-[#73008F]"
-              ></input>
+              <input type="text" className="rounded-[3px] h-[45px]"></input>
             </div>
-            <div className="login-password flex flex-col gap-1 relative">
+            <div className="login-password flex flex-col gap-1">
               <h6 className="font-inter text-[#73008F] text-[.8125rem]">
                 Password
               </h6>
-              <input
-                type={showPassword ? "text" : "password"}
-                className="rounded-[3px] h-[45px] focus:outline-none focus:border-[2px] focus:border-[#72008f4f] pl-9 text-[15px] font-inter caret-[#73008F]"
-              ></input>
-              <i
-                className="far fa-eye absolute float-left mt-[39px] ml-3 text-[14px] cursor-pointer"
-                id="togglePassword"
-                onClick={togglePasswordVisibility}
-              ></i>
+              <input type="text" className="rounded-[3px] h-[45px]"></input>
             </div>
           </div>
         </form>
